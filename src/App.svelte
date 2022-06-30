c4ca430ae895c7359519469435327d8ddc01c551
Mohammed Alshahrani <mmogib@gmail.com>
2022-06-30 16:47:35 +0300

    added manual update

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,34 @@
 <script lang="ts">
   import Home from "./lib/Home.svelte";
+  import { checkUpdate, installUpdate } from "@tauri-apps/api/updater";
+  import { relaunch } from "@tauri-apps/api/process";
+  import { message as diaMsg, confirm } from "@tauri-apps/api/dialog";
+  import { onMount } from "svelte";
+  onMount(async () => {
+    try {
+      const { shouldUpdate, manifest } = await checkUpdate();
+      if (shouldUpdate) {
+        const ok = await confirm(
+          `A new version (${manifest.version}) is available. Do you want to update now?`,
+          {
+            title: "Update",
+            type: "warning",
+          }
+        );
+        if (ok) {
+          await installUpdate();
+          await diaMsg("Update complete, your app will relaunce now.");
+          await relaunch();
+        }
+        // install complete, restart app
+      }
+    } catch (error) {
+      await diaMsg(`Problem updaing ${error}`, {
+        title: "Updater",
+        type: "error",
+      });
+    }
+  });
 </script>
 
 <main class="relative container mx-auto p-4 my-5 ">
