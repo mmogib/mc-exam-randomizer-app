09bee3b9ea246cb99d985a7f196bf4ad979bd00a
Mohammed Alshahrani <mmogib@gmail.com>
2022-07-05 17:45:41 +0300

    updated version

---
+++ docs/.vuepress/components/AppVersion.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Download and install the latest version of the app from
+    <a :href="version_link" target="_blank" rel="noopener noreferrer">here</a>
+  </div>
+</template>
+<script>
+export default {
+  data: () => ({
+    version: "0.2.2",
+  }),
+  computed: {
+    version_link() {
+      return `https://github.com/mmogib/mc-exam-randomizer-app/releases/download/v${this.version}/MC.Exam.Randomizer_${this.version}_x64_en-US.msi`;
+    },
+  },
+};
+</script>
